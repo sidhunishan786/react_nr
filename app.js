af255f650ea2b75821813318d13c5257d6c4b731
sidhunishan786 <sidhunishan786@gmail.com>
2023-03-26 18:21:32 +0530

    jshelloworld

--- app.js
+++ app.js
@@ -1,41 +1,4 @@
-
-const root=ReactDOM.createRoot(document.getElementById('root'));
-
-
-const parent=React.createElement('div',
-{id:'parent'},
-[React.createElement('div',
-{id:'child'},
-[React.createElement('h1',
-{id:'hehe'},'im h1 tag'),
-React.createElement('h1',
-{id:'h2o'},
-"2nd heading ")])
-,
-React.createElement('div',
-{id:'child'},
-[React.createElement('h1',
-{id:'hehe'},'im h1 tag'),
-React.createElement('h1',
-{id:'h2o'},
-"2nd heading ")])]);
-root.render(parent);
-/*
-<div id="parent">
-<div id="child">
-React element is object and 
-<h1></h1>
-<h1></h1>
-</div>
-
-</div>
-
-<div id="parent">
-<div id="child">
-React element is object and 
-<h1></h1>
-<h1></h1>
-</div>
-
-</div>
-*/
+const x=document.getElementById('main');
+const y=document.createElement('h1');
+y.innerHTML="hello world!!!!!!!";
+x.appendChild(y);
