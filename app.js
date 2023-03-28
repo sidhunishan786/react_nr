b1b82cc5bd42bb50484b53fefa7de1e03af63abf
sidhunishan786 <sidhunishan786@gmail.com>
2023-03-28 23:43:22 +0530

    28march

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 import React from "react";
 import  ReactDOM from "react-dom/client";
-
-
-const x={color : 'red',padding:'100px'};
-const y=React.createElement("div",{},[React.createElement('h1',{style:x},"hello")]);
-const root=ReactDOM.createRoot(document.getElementById('root'));
-root.render(y);
+const ob={'color':'red'};
+// react element
+const heading=React.createElement("h1",{'style':ob},"hello ");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
