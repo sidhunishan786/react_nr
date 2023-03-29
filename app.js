6709752e80452704dcc1c773efec5d8007878885
sidhunishan786 <sidhunishan786@gmail.com>
2023-03-29 20:10:53 +0530

    29thmarch2023

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 import React from "react";
 import  ReactDOM from "react-dom/client";
-const ob={'color':'red'};
-// react element
-const heading=React.createElement("h1",{'style':ob},"hello ");
+
+const Jsxheading=()=>(<h1 className="head" tabIndex={5}>this is jsx</h1>);
+
+console.log(Jsxheading);
+const HeadingComponent=()=>{
+     return (<h1 className="headingreal">
+          <Jsxheading/>
+          this is my functional components</h1>)
+};
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+//components in react
+//class Based components OLD
+//function Based components NEW
+
+root.render(<HeadingComponent/>);
+
+
