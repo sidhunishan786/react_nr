a942a5f4c2b1ed0c02bff3ed55baa39ed73c2af9
sidhunishan786 <sidhunishan786@gmail.com>
2023-03-30 01:21:21 +0530

    30march2023

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 import React from "react";
-import  ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const Jsxheading=()=>(<h1 className="head" tabIndex={5}>this is jsx</h1>);
-
+const Jsxheading = () => (
+  <h1 className="head" tabIndex={5}>
+    this is jsx
+  </h1>
+);
+const number = 1000;
 console.log(Jsxheading);
-const HeadingComponent=()=>{
-     return (<h1 className="headingreal">
-          <Jsxheading/>
-          this is my functional components</h1>)
+const HeadingComponent = () => {
+  return (
+    <h1 className="headingreal">
+      {number}
+      this is my functional components
+    </h1>
+  );
 };
 
-const root=ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
 //components in react
 //class Based components OLD
 //function Based components NEW
 
-root.render(<HeadingComponent/>);
-
-
+root.render(<HeadingComponent />);
+root2.render(Jsxheading());
