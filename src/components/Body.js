caf6fd68d39eb71532ceb2dd5aa57ec5f34a33e2
sidhunishan786 <sidhunishan786@gmail.com>
2023-04-03 11:58:14 +0530

    3april2023

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,24 +3,37 @@ import RestaurantCard from "./RestaurantCard";
 import { useState } from "react";
 
 
+
+
+
+
+
 import reslist from "../utils/mockData";
 const Body=()=>{
-    console.log(reslist.length);
+  const [listOfRes,setlistOfRes]=useState(reslist)
+
+  
     return (
         <>
+       
         <div id="filter"> <button onClick={()=>{
             //filter logic here 
-            
-            reslist=reslist.filter(res=>
+            console.log(listOfRes.length);
+           
+
+            const filteredList=listOfRes.filter(res=>
                 res.data.avgRating>4);
-            console.log(reslist.length);    
+                setlistOfRes(filteredList);
+            
+                console.log(listOfRes.length);
+                
 
         }} id="filter-btn"> Top rated Restaurants</button> </div>
         
       
     <div id="restaurants">
     {
-      reslist.map((res)=>{
+      listOfRes.map((res)=>{
         return <RestaurantCard key={res.data.id} resData={res}/>
       })
     }
