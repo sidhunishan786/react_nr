40dfbb29a041962d3be8f7563f62e3847f053637
sidhunishan786 <sidhunishan786@gmail.com>
2023-04-13 23:50:53 +0530

    props drilling

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,10 +1,64 @@
+import { useState } from "react";
+import { useAsyncError } from "react-router-dom";
+
+
+const Section=({title,description,visible,setVisible})=>{
+   
+
+
+    return (
+        <div>
+            <h1>{title}</h1>
+
+            <button onClick={() => (setVisible())} className="hover:pointer-events-auto border-4 border-red-500 bg-green-300 text-cyan-700">Toggle</button>
+            <p>
+                {visible?description:""}
+            </p>
+
+        </div>
+        
+
+    );
+}
+
+
 const Instamart=()=>{
-    return (<div>
+    const [sectionconfig,setSectionConfig]=useState({
+        showAbout:false,
+        showTeam:false,
+        showCareer:false
+    });
+    return (
+    <div>
 
-        <h1>instamart</h1>
-        <h1>100s of components inside it</h1>
+        <Section key={12} setVisible={()=>{
+            setSectionConfig({
+                showAbout:true,
+        showTeam:false,
+        showCareer:false
+            })
 
-    </div>);
+        }} title={"about instamart"} visible={sectionconfig.showAbout} description={"Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It's not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.” As Cicero would put it, “Um, not so fast.” The placeholder text, beginning with the line “Lorem ipsum dolor sit amet, consectetur adipiscing elit”, looks like Latin because in its youth, centuries ago, it was Latin. Richard McClintock, a Latin scholar from Hampden-Sydney College, is credited with discovering the source behind the ubiquitous filler text. In seeing a sample of lorem ipsum, his interest was piqued by consectetur—a genuine, albeit rare, Latin word. Consulting a Latin dictionary led McClintock to a passage from De Finibus Bonorum et Malorum (“On the Extremes of Good and Evil”), a first-century B.C. text from the Roman philosopher Cicero. In particular, the garbled words of lorem ipsum bear an unmistakable resemblance to sections 1.10.32–33 of Cicero's work, with the most notable passage excerpted below: “Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.” A 1914 English translation by Harris Rackham reads: “Nor is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure.” McClintock's eye for detail certainly helped narrow the whereabouts of lorem ipsum's origin, however, the “how and when” still remain something of a mystery, with competing theories and timelines."}/>
+        <Section setVisible={()=>{
+            setSectionConfig({
+                showAbout:false,
+        showTeam:true,
+        showCareer:false
+            })
+
+        }} key={2} title={"teams of instamart"} visible={sectionconfig.showTeam} description={"Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It's not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.” As Cicero would put it, “Um, not so fast.” The placeholder text, beginning with the line “Lorem ipsum dolor sit amet, consectetur adipiscing elit”, looks like Latin because in its youth, centuries ago, it was Latin. Richard McClintock, a Latin scholar from Hampden-Sydney College, is credited with discovering the source behind the ubiquitous filler text. In seeing a sample of lorem ipsum, his interest was piqued by consectetur—a genuine, albeit rare, Latin word. Consulting a Latin dictionary led McClintock to a passage from De Finibus Bonorum et Malorum (“On the Extremes of Good and Evil”), a first-century B.C. text from the Roman philosopher Cicero. In particular, the garbled words of lorem ipsum bear an unmistakable resemblance to sections 1.10.32–33 of Cicero's work, with the most notable passage excerpted below: “Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.” A 1914 English translation by Harris Rackham reads: “Nor is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure.” McClintock's eye for detail certainly helped narrow the whereabouts of lorem ipsum's origin, however, the “how and when” still remain something of a mystery, with competing theories and timelines."}/>
+        <Section setVisible={()=>{
+            setSectionConfig({
+                showAbout:false,
+        showTeam:false,
+        showCareer:true
+            })
+
+        }} key={122} title={"carrers"} visible={sectionconfig.showCareer} description={"Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It's not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.” As Cicero would put it, “Um, not so fast.” The placeholder text, beginning with the line “Lorem ipsum dolor sit amet, consectetur adipiscing elit”, looks like Latin because in its youth, centuries ago, it was Latin. Richard McClintock, a Latin scholar from Hampden-Sydney College, is credited with discovering the source behind the ubiquitous filler text. In seeing a sample of lorem ipsum, his interest was piqued by consectetur—a genuine, albeit rare, Latin word. Consulting a Latin dictionary led McClintock to a passage from De Finibus Bonorum et Malorum (“On the Extremes of Good and Evil”), a first-century B.C. text from the Roman philosopher Cicero. In particular, the garbled words of lorem ipsum bear an unmistakable resemblance to sections 1.10.32–33 of Cicero's work, with the most notable passage excerpted below: “Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.” A 1914 English translation by Harris Rackham reads: “Nor is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure.” McClintock's eye for detail certainly helped narrow the whereabouts of lorem ipsum's origin, however, the “how and when” still remain something of a mystery, with competing theories and timelines."}/>
+        
+
+    </div>
+    );
 };
 
 export default Instamart;
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,7 +2,7 @@ const Shimmer=()=>{
     return (
         <div id="shimmers">
            
-           {Array(20).fill("").map((e)=>(<div key={e} className="w-20 bg-slate-500 flex flex-wrap px-6 py-6"></div>))}
+           {Array(30).fill("").map((e)=>(<div key={e} className="  h-2 w-6 bg-slate-500 flex flex-wrap justify-between px-2 py-2"></div>))}
         </div>
 
     )
